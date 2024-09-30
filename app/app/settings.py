1ca1077ddfcf01cde8f66bd0a89c2b7d1be4ada1
Samuel-Tefera <samuelteferab@gmail.com>
2024-09-30 13:38:13 +0300

    update user model and installed apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'user',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
