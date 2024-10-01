0d8d2150b6a305ee914cd3ef8e8b890fe4e87c18
Samuel-Tefera <samuelteferab@gmail.com>
2024-10-01 19:32:26 +0300

    add url for task views

---
+++ app/task/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+router = DefaultRouter()
+router.register('task', views.TaskAPIViewSets)
+
+app_name = 'task'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
